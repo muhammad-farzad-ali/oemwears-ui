99747e5e0779464d7bd757944fb900c0b4a525a1
OEMWears Dev <dev@oemwears.local>
2026-06-05 16:12:42 +0200

    T19: home composition - drop CertificationsSection, add PartnerCountriesSection, rename to FeaturedApparelSection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
   import { t } from '$lib/content';
   import { pageTitle } from '$lib/utils/seo';
   import HeroSection from '$lib/components/home/HeroSection.svelte';
+  import PartnerCountriesSection from '$lib/components/home/PartnerCountriesSection.svelte';
   import UspsSection from '$lib/components/home/UspsSection.svelte';
-  import FeaturedProductsSection from '$lib/components/home/FeaturedProductsSection.svelte';
-  import CertificationsSection from '$lib/components/home/CertificationsSection.svelte';
+  import FeaturedApparelSection from '$lib/components/home/FeaturedApparelSection.svelte';
   import TestimonialsSection from '$lib/components/home/TestimonialsSection.svelte';
   import CtaSection from '$lib/components/home/CtaSection.svelte';
 </script>
@@ -16,9 +16,9 @@
 
 <div class="flex flex-col">
   <HeroSection />
+  <PartnerCountriesSection />
   <UspsSection />
-  <FeaturedProductsSection />
-  <CertificationsSection />
+  <FeaturedApparelSection />
   <TestimonialsSection />
   <CtaSection />
 </div>
